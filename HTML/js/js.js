8b795efa08b73e84840d53f9525a30778dfcbdf0
TieuLien <41802972+TieuLien@users.noreply.github.com>
2020-12-09 23:29:35 +0700

    Update js.js

--- HTML/js/js.js
+++ HTML/js/js.js
@@ -22,11 +22,13 @@ var swiper = new Swiper('.swiper-container', {
 	  spaceBetween: 20,
 	},
 	768: {
-	  slidesPerView: 3,
+	  slidesPerView: 2,
+	  slidesPerColumn: 2,
 	  spaceBetween: 20,
 	},
 	812: {
 	  slidesPerView: 3,
+	  slidesPerColumn: 1,
 	  spaceBetween: 20,
 	},
 	1024: {
