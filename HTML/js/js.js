46d5f10405d5311f30b01cdbc98445eda4fca810
TieuLien <41802972+TieuLien@users.noreply.github.com>
2020-12-09 23:26:04 +0700

    a

--- HTML/js/js.js
+++ HTML/js/js.js
@@ -19,12 +19,10 @@ var swiper = new Swiper('.swiper-container', {
 	},
 	640: {
 	  slidesPerView: 3,
-	  slidesPerColumn: 1,
 	  spaceBetween: 20,
 	},
 	768: {
 	  slidesPerView: 2,
-	  slidesPerColumn: 2,
 	  spaceBetween: 20,
 	},
 	812: {
